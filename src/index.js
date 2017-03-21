31d22a62639f12da8e3e33e6631c468cfed6146f
Unknown <akred@live.com>
2017-03-21 23:56:01 +0100

    Step 13 : Youtube Search API Signup

--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ const App = function() {
     return <div>Hi!</div>;
 }
 // Take this component's generated HTML and put it on the page (in the DOM)
-React.render(App);
+React.render(<App/>, document.querySelector('.container'));
