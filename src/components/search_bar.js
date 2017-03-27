5d28dd2e33e93c59785563f405948c6d019e2746
akred <akred@live.com>
2017-03-27 13:48:42 +0200

    Step 14 : Export Statements

---
+++ src/components/search_bar.js
@@ -0,0 +1,7 @@
+import React from 'react'; // needed for JSX notation
+
+const SearchBar = () => {
+    return <input />; // React.createElement
+};
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+// Youtube work account
+const API_KEY = 'AIzaSyD4tH_7Jd9KPn3nPpSF1hdYBJ52LojpXv0';
+
 // Create a new component. This component should produce some HTML
 const App = function() {
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 // Take this component's generated HTML and put it on the page (in the DOM)
 React.render(<App/>, document.querySelector('.container'));
