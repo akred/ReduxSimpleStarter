760f7c5d9c9f8d6d2e6bb5ecdc3c564f5c8066d6
akred <akred@live.com>
2017-06-08 17:21:20 +0200

    Step 17 : Introduction to state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react'; // needed for JSX notation
 //const Component = React.Component;
 
 class SearchBar extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {term : ''};
+    }
+    
     render() {
         return <input onChange={this.onInputChange} />;
         //return <input onChange={(event) => {console.log(event.target.value)}} />;
