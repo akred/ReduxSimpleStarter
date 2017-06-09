846436657930b8d592de175df3fc7877d59dbcfb
Réda AKERMI <rakermi@qualys.com>
2017-06-09 16:33:31 +0200

    Step 1: Youtube Seach Response

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 // Youtube work account
 const API_KEY = 'AIzaSyD4tH_7Jd9KPn3nPpSF1hdYBJ52LojpXv0';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+    console.log(data);
+});
 
 // Create a new component. This component should produce some HTML
 const App = function() {
