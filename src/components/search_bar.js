1a26e5bad0831671ce2b0c5d20276353802d5de3
Réda AKERMI <rakermi@qualys.com>
2017-06-12 17:27:10 +0200

    Step 31: Searching for videos

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -12,13 +12,16 @@ class SearchBar extends React.Component {
         //return <input onChange={this.onInputChange} />;
         return (
             <div className="search-bar">
-                <input onChange={(event) => this.setState({term : event.target.value})} />
+                <input
+                    value={this.state.term}
+                    onChange={event => this.onInputChange(event.target.value)} />
             </div>
         );
     }
 
-    onInputChange(event) {
-        console.log(event.target.value);
+    onInputChange(term) {
+        this.setState({term});
+        this.props.onSearchTermChange(term);
     }
 }
 
