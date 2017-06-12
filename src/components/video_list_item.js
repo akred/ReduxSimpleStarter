881330dcc9a1456d0136e421724d23183d91a001
Réda AKERMI <rakermi@qualys.com>
2017-06-12 16:28:38 +0200

    Step 29 : Video Selection

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const VideoListItem = ({ video }) => {
+const VideoListItem = ({ video, onVideoSelect }) => {
 
     /*
      * We use {video}on argument funcion instead of "props" argument
@@ -11,7 +11,7 @@ const VideoListItem = ({ video }) => {
     //console.log(video);
 
     return (
-        <li className="list-group-item">
+        <li onClick={() => onVideoSelect(video)} className="list-group-item">
             <div className="video-list media">
                 <div className="media-left">
                     <img className="media-object" src={imageUrl} />
