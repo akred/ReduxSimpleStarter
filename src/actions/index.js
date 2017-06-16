2ff599cc2e1e44a2145ea3ddc5598ccbea030143
akred <akred@live.com>
2017-06-16 17:26:26 +0200

    Step 58 : Redux-Promise in Practice

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = 'c8f74c3f4a4f4974bdb1a6f600d9c78a';
-const ROOL_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOL_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
