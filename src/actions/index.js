3da36601ddfb1fa6eb869c6f9d31bedcddc37291
akred <akred@live.com>
2017-06-16 16:58:48 +0200

    Step 57 : Ajax Requests with Axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,15 @@
+import axios from 'axios';
+
 const API_KEY = 'c8f74c3f4a4f4974bdb1a6f600d9c78a';
+const ROOL_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOL_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
