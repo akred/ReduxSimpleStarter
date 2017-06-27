a3f22cbc37ddbcf3c47ae7f9aaef8fc1eebd024d
Akred <akred@live.com>
2017-06-27 16:56:49 +0200

    Step 60 : Avoiding State Mutations in Reducers

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,18 @@
-export default function(state = null, action) {
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+    switch (action.type) {
+        case FETCH_WEATHER:
+            // concat is different from push
+            // we don't use push because we mustn't mutates the state here
+            // "push" mutates the state / concat creates a new version (instance) of the state
+            
+            // return state.concat([action.payload.data]);
+            // ES6 syntax
+            return [ action.payload.data, ...state ]; // [city, city, city] and NOT [city, [city, city]]
+            break;
+        default:
+
+    }
     return state;
 }
