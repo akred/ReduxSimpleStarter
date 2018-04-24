ab7e4c20832c08375802b62a6ad7f06e01f8a2b3
akred <akred@live.com>
2018-04-24 15:02:24 +0200

    Step 62 : Mapping props to a render helper

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -2,22 +2,35 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class WeatherList extends Component {
-    render() {
-        return (
-            <table className="table table-hover">
-                <thead>
-                    <tr>
-                        <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
-                    </tr>
-                </thead>
-                <tbody>
-                </tbody>
-            </table>
-        );
-    }
+
+  renderWeather(cityData) {
+
+    const name = cityData.city.name;
+    
+    return (
+      <tr>
+        <td key={name}>{name}</td>
+      </tr>
+    );
+  }
+
+  render() {
+      return (
+          <table className="table table-hover">
+              <thead>
+                  <tr>
+                      <th>City</th>
+                      <th>Temperature</th>
+                      <th>Pressure</th>
+                      <th>Humidity</th>
+                  </tr>
+              </thead>
+              <tbody>
+                {this.props.weather.map(this.renderWeather)}
+              </tbody>
+          </table>
+      );
+  }
 }
 
 
