82e2fbe9eedfd30cf0eeb8c89feae7874e6e3b27
akred <akred@live.com>
2018-04-25 14:13:22 +0200

    Step 68 : Google Maps Integration Continued

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -1,13 +1,12 @@
-import React, Component from 'react';
-
+import React, { Component } from 'react';
 
 class GoogleMap extends Component {
   componentDidMount() {
     new google.maps.Map(this.refs.map, {
         zoom: 12,
         center: {
-          lat: this.proprs.lat,
-          lng: this.proprs.lon
+          lat: this.props.lat,
+          lng: this.props.lon
         }
     });
   }
