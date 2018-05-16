8eb226707802c4d7be03afb61da02ff55ec790cf
akred <akred@live.com>
2018-05-16 16:50:00 +0200

    Step 86 : Forms and Form Submission

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import PostsReducer from './reducer_posts';
+import { reducer as formReducer } fom 'redux-form';
 
 const rootReducer = combineReducers({
-  posts: PostsReducer
+  posts: PostsReducer,
+  form: formReducer
 });
 
 export default rootReducer;
