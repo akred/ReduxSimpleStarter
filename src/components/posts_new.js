26a616b436f09249a2011d292f15364e4de80a28
akred <akred@live.com>
2018-05-17 17:01:06 +0200

    Step 88 : Passing Control to Redux Form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -3,22 +3,25 @@ import { reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
   render() {
+    // ES 6 syntax
+    const { fields: { title, categories, content }, handleSubmit } = this.props;// eq : const handleSubmit = this.props.handleSubmit;
+    // eq : sconst title = this.props.fields.title;
     return (
-      <form>
+      <form onSubmit={handleSubmit}>
         <h3>Create a new post</h3>
         <div className="form-group">
           <label>Title</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" {...title}/>
         </div>
 
         <div className="form-group">
           <label>Categories</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" {...categories}/>
         </div>
 
         <div className="form-group">
           <label>Content</label>
-          <textarea className="form-control" />
+          <textarea className="form-control" {...content}/>
         </div>
 
         <button type="submit" className="btn btn-primary">Submit</button>
