67e97944e30710468d407fbaf6a68c20568860b9
akred <akred@live.com>
2018-05-22 12:34:21 +0200

    Step 89 : CreatePost Action Creator

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+import { createPost } from '../actions/index';
 import { reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
   render() {
     // ES 6 syntax
     const { fields: { title, categories, content }, handleSubmit } = this.props;// eq : const handleSubmit = this.props.handleSubmit;
-    // eq : sconst title = this.props.fields.title;
+    // eq :sconst title = this.props.fields.title;
     return (
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(this.props.createPost)}>
         <h3>Create a new post</h3>
         <div className="form-group">
           <label>Title</label>
@@ -29,11 +30,13 @@ class PostsNew extends Component {
     );
   }
 }
+// connect : first argument is mapStateToProps, 2nd is mapDispatchToProps
+// reduxForm : 1st is a form config, 2nd is mapStateToProps, 3rd is mapDispatchToProps
 
 export default reduxForm({
   form: 'PostsNewForm',
   fields: ['title', 'categories', 'content']
-})(PostsNew);
+}, null, { createPost })(PostsNew);
 
 
 // user types something in... record it on application state
